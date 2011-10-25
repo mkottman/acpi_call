8530fdb108f05556d323f331c8d3e5dbd5e20b2e
Lekensteyn <lekensteyn@gmail.com>
2011-10-25 17:21:01 +0200

    Revert "Removed unused files"
    This reverts commit 7b70c3b2777e7d385a763bdca4a0dfb1ea02dc02.

---
+++ test_off.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+if lsmod | grep -q acpi_call; then
+methods="
+\_SB.PCI0.P0P1.VGA._OFF
+\_SB.PCI0.P0P2.VGA._OFF
+\_SB_.PCI0.OVGA.ATPX
+\_SB_.PCI0.OVGA.XTPX
+\_SB.PCI0.P0P3.PEGP._OFF
+\_SB.PCI0.P0P2.PEGP._OFF
+\_SB.PCI0.P0P1.PEGP._OFF
+\_SB.PCI0.MXR0.MXM0._OFF
+\_SB.PCI0.PEG1.GFX0._OFF
+\_SB.PCI0.PEG0.GFX0.DOFF
+\_SB.PCI0.PEG1.GFX0.DOFF
+\_SB.PCI0.PEG0.PEGP._OFF
+\_SB.PCI0.XVR0.Z01I.DGOF
+\_SB.PCI0.PEGR.GFX0._OFF
+\_SB.PCI0.PEG.VID._OFF
+\_SB.PCI0.PEG0.VID._OFF
+\_SB.PCI0.P0P2.DGPU._OFF
+\_SB.PCI0.P0P4.DGPU.DOFF
+\_SB.PCI0.IXVE.IGPU.DGOF
+\_SB.PCI0.RP00.VGA._PS3
+\_SB.PCI0.RP00.VGA.P3MO
+\_SB.PCI0.GFX0.DSM._T_0
+\_SB.PCI0.LPC.EC.PUBS._OFF
+\_SB.PCI0.P0P2.NVID._OFF
+\_SB.PCI0.P0P2.VGA.PX02
+"
+
+    for m in $methods; do
+echo -n "Trying $m: "
+        echo $m > /proc/acpi/call
+        result=$(cat /proc/acpi/call)
+        case "$result" in
+        Error*)
+            echo "failed"
+        ;;
+        *)
+            echo "works!"
+            break
+        ;;
+        esac
+done
+else
+echo 'The acpi_call module is not loaded'
+    exit 1
+fi
